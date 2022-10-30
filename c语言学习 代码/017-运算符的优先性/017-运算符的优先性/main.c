38a82483a3fa0a88a1edf3eb6f3ffeaf1ab48d45
weiwei <3012937404@qq.com>
2022-10-30 08:08:57 +0800

    update

--- c语言学习 代码/017-运算符的优先性/017-运算符的优先性/main.c
+++ c语言学习 代码/017-运算符的优先性/017-运算符的优先性/main.c
@@ -3,7 +3,7 @@ int main(){
     int a,b,c,d;
     a = b = c = d = 100;
     a += b += c+= d = 100;
-    printf("%d,%d,%d,%d\n",a,d,c,d);
+    printf("%d,%d,%d,%d\n",a,b,c,d);
     int n = 1;
     printf("%d\n",(++n)+(++n)+(++n));
 }
